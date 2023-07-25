a7b293443f8a637b500a24e73e9ad39aac929fd5
Doctor Pranathi <126224688+Pranathi0000@users.noreply.github.com>
2023-07-25 12:01:12 +0530

    Add files via upload

---
+++ NodeSchool(js)/function-arguments.js
@@ -0,0 +1,5 @@
+function math(ag1,ag2,ag3)
+{
+    return ag1+(ag2*ag3);
+}
+console.log(math(53,61,67));
---
+++ NodeSchool(js)/looping-through-arrays.js
@@ -0,0 +1,11 @@
+let pets=['cat', 'dog', 'rat'];
+
+for(let i=0;i<pets.length;i++)
+{
+    pets[i]+='s';
+}
+
+for(let i=0;i<pets.length;i++)
+{
+    console.log(pets[i]);
+}
